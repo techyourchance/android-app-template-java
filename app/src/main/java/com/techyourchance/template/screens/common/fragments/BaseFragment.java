b6fdce0f15f563c444afba8cc1055511cae227b8
Vasiliy Zukanov <techyourchance@gmail.com>
2017-05-30 09:53:40 +0300

    Addition of getControllerComponent method to BaseFragment

--- app/src/main/java/com/techyourchance/template/screens/common/fragments/BaseFragment.java
+++ app/src/main/java/com/techyourchance/template/screens/common/fragments/BaseFragment.java
@@ -1,7 +1,29 @@
 package com.techyourchance.template.screens.common.fragments;
 
+import android.support.annotation.UiThread;
 import android.support.v4.app.Fragment;
+
+import com.techyourchance.template.MyApplication;
+import com.techyourchance.template.common.dependencyinjection.controller.ControllerComponent;
+import com.techyourchance.template.common.dependencyinjection.controller.ControllerModule;
+import com.techyourchance.template.common.dependencyinjection.controller.ViewMvcModule;
 
 public abstract class BaseFragment extends Fragment {
 
+    private ControllerComponent mControllerComponent;
+
+    /**
+     * @return controller injector of type {@link ControllerComponent}
+     */
+    @UiThread
+    protected ControllerComponent getControllerComponent() {
+        if (mControllerComponent == null) {
+            mControllerComponent =
+                    ((MyApplication)getActivity().getApplication()).getApplicationComponent()
+                            .newControllerComponent(
+                                    new ControllerModule(getActivity(), getActivity().getSupportFragmentManager()),
+                                    new ViewMvcModule());
+        }
+        return mControllerComponent;
+    }
 }
