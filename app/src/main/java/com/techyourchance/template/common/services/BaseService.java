7850c0a84a958c2fbaf54a98989404c29c1ea8f8
Vasiliy Zukanov <techyourchance@gmail.com>
2017-06-03 14:30:59 +0300

    Introduction of BaseService

---
+++ app/src/main/java/com/techyourchance/template/common/services/BaseService.java
@@ -0,0 +1,35 @@
+package com.techyourchance.template.common.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
+
+import com.techyourchance.template.MyApplication;
+import com.techyourchance.template.common.dependencyinjection.service.ServiceComponent;
+import com.techyourchance.template.common.dependencyinjection.service.ServiceModule;
+
+public class BaseService extends Service {
+
+    private ServiceComponent mServiceComponent;
+    
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    /**
+     * @return service injector of type {@link ServiceComponent}
+     */
+    @UiThread
+    protected ServiceComponent getServiceComponent() {
+        if (mServiceComponent == null) {
+            mServiceComponent =
+                    ((MyApplication)getApplication()).getApplicationComponent()
+                            .newServiceComponent(new ServiceModule(this));
+        }
+        return mServiceComponent;
+    }
+}
